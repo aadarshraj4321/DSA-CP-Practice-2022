5cf8dd032bf8f50512159a6c2a86e93f6e74d2df
aadarshraj <aadarshraj4321@gmail.com>
2023-02-21 21:01:12 +0530

    OOP_concept

--- c++_OOP/Student.cpp
+++ c++_OOP/Student.cpp
@@ -4,5 +4,24 @@ class Student
 {
 public:
 	int rollNumber;
+private:
 	int age;
+
+public:
+	void displayData()
+	{
+		std::cout << "Roll Number : " << rollNumber << " Age : " << age << '\n';
+	}
+
+	int getAge()
+	{
+		return age;
+	}
+
+	void setAge(int a)
+	{
+		if(a < 0)
+			return;
+		age = a;
+	}
 };
