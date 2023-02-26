af930fdb8f9fec63b75b9c98f5b4b9ae4b1bfbb1
aadarshraj <aadarshraj4321@gmail.com>
2023-02-26 11:46:05 +0530

    OOP

--- c++_OOP/Student.cpp
+++ c++_OOP/Student.cpp
@@ -8,6 +8,27 @@ private:
 	int age;
 
 public:
+
+	// Default Constructor
+	Student()
+	{
+		std::cout << "Constructor Called" << '\n';
+	}
+
+	// Parametrized Constructor
+	Student(int r)
+	{
+		std::cout << "Constructor 2 Called" << '\n';
+		rollNumber = r;
+	}
+
+	Student(int r, int a)
+	{
+		std::cout << "Constructor 3 Called" << '\n';
+		rollNumber = r;
+		age = a;
+	}
+
 	void displayData()
 	{
 		std::cout << "Roll Number : " << rollNumber << " Age : " << age << '\n';
---
+++ c++_OOP/constructor.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include "Student.cpp"
+
+int main()
+{
+
+	Student s1;
+	s1.displayData();
+
+	Student s2;
+
+	Student *s3 = new Student;
+	s3->displayData();
+
+	std::cout << "Parametrized Constructor Constructor" << '\n';
+
+	Student s4(111);
+	s4.displayData();
+
+	Student *s5 = new Student(222);
+	s5->displayData();
+
+	std::cout << "Parametrized Constructor with 2 paramter Called" << '\n';
+	Student s6(111, 21);
+	s6.displayData();
+
+	Student *s7 = new Student(222, 18);
+	s7->displayData();
+
+	return 0;
+}
