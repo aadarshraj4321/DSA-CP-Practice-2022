1b7758551a32dab7cd54d296676c41901749050b
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-04-09 18:33:54 +0530

    find_the_fine

---
+++ ds-algo-question/math/find_the_fine.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    long long int totalFine(int n, int date, int car[], int fine[])
+    {
+        long long int total = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(date % 2 == 0)
+            {
+                if(car[i] % 2 != 0)
+                {
+                    total += fine[i];
+                }
+            }
+            
+            else
+            {
+                if(car[i] % 2 == 0)
+                {
+                    total += fine[i];
+                }
+            }
+        }
+        return total;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i, date;
+        cin >> n >> date;
+        int car[n], fine[n];
+        
+        for (i = 0; i < n; i++){
+            cin >> car[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> fine[i];
+        }
+        Solution obj;
+        cout << obj.totalFine(n, date, car, fine) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
