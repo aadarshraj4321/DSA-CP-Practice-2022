1babd6be4748226eec4879f8f3299a6fea5edf7f
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-04-09 18:47:30 +0530

    2d-array-questions

---
+++ ds-algo-question/2-d-array/search_a_2d_matrix.cpp
@@ -0,0 +1,19 @@
+#include<bits/stc++.h>
+
+
+class Solution {
+public:
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < matrix[i].size(); j++)
+            {
+                std::cout << matrix[i][j] << '\n';
+                if(matrix[i][j] == target)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
