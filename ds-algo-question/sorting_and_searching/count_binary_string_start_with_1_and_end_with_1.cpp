a083c03464f2c34431699e6987372f84e5d92cd4
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-04-30 20:59:03 +0530

    string_binary_string_problem

---
+++ ds-algo-question/sorting_and_searching/count_binary_string_start_with_1_and_end_with_1.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to count the number of substrings that start and end with 1.
+    long binarySubstring(int n, string a){
+        
+        long long count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] == '1')
+            {
+                count++;
+            }
+        }
+        return (count * (count - 1)) / 2;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n,count;
+    string a;
+    cin>>t;
+    while(t--)
+    {
+        count = 0;
+        cin >> n;
+        cin >> a;
+        Solution obj;
+        cout << obj.binarySubstring(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
