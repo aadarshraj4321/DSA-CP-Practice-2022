1fbc75b37361f25baff6dd528ffb1910e52c4a60
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-06-21 17:01:11 +0530

    c++_OOPS

--- c++_OOP/main_student.cpp
+++ c++_OOP/main_student.cpp
@@ -15,7 +15,10 @@ int main()
 
 	s1.age = 19;
 	s1.rollNumber = 101;
-	std::cout << s1.age << " " << s1.rollNumber << '\n';
+	//s1.id = 1;
+	s1.setID(1);
+	std::cout << s1.age << " " << s1.rollNumber << " " << s1.getID() << '\n';
+	//s1.display();
 
 
 	std::cout << "Dynamically Objects" << '\n';
@@ -24,6 +27,8 @@ int main()
 	Student * s5 = new Student;
 	s5->age = 20;
 	s5->rollNumber = 1011;
-	std::cout << s5->age << " " << s5->rollNumber << '\n';
+	s5->setID(2);
+	std::cout << s5->age << " " << s5->rollNumber << " " << s5->getID() << '\n';
+	//s5->display();
 
 }
--- c++_OOP/student2.cpp
+++ c++_OOP/student2.cpp
@@ -6,4 +6,27 @@ class  Student
 public:
 	int age;
 	int rollNumber;
+
+private:
+	int id;
+
+
+public:
+
+	void display()
+	{
+		std::cout << age << " " << rollNumber << " " << id;
+	}
+
+
+	int  getID()
+	{
+		return id;
+	}
+
+
+	void setID(int a)
+	{
+		id = a;
+	}
 };
