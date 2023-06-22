f98537b26e3d6c0c9784db5dd3ef6489751a765e
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-06-22 10:59:08 +0530

    class_fraction_oops

---
+++ c++_OOP/Fraction.cpp
@@ -0,0 +1,56 @@
+
+
+class Fraction
+{
+	private:
+		int numerator;
+		int denominator;
+
+	public:
+		Fraction(int numerator, int denominator)
+		{
+			this->numerator = numerator;
+			this->denominator = denominator;
+		}
+
+
+		void print()
+		{
+			std::cout << this->numerator << "/" << this->denominator << '\n';
+		}
+
+
+
+		void simplify()
+		{
+			int gcd = 1;
+			int j = std::min(this->numerator, this->denominator);
+
+			for(int i = 1; i <= j; i++)
+			{
+				if(this->numerator % i == 0 && this->denominator % i == 0)
+				{
+					gcd = i;
+				}
+			}
+
+			this->numerator = this->numerator / gcd;
+			this->denominator = this->denominator / gcd;
+		}
+
+
+		void add(Fraction f2)
+		{
+			int lcm = this->denominator * f2.denominator;
+			int x = lcm / this->denominator;
+			int y = lcm / f2.denominator;
+
+			int  num = x * this->numerator + (y * f2.numerator);
+
+			this->numerator = num;
+			this->denominator = lcm;
+
+			simplify();
+		}
+
+};
---
+++ c++_OOP/FractionUse.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+#include "Fraction.cpp"
+
+
+
+
+
+
+int main()
+{
+
+	Fraction f1(10, 5);
+	Fraction f2(20, 5);
+
+	f1.print();
+	f1.add(f2);
+	f1.print();
+
+
+	return 0;
+}
