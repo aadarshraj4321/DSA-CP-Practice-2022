436ec58c6463cb4414cb4ab657930d04079f1763
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-06-28 13:53:11 +0530

    stl_map

---
+++ c++_OOP/stl/map.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+
+
+void thisIsMap()
+{
+	// map is container which store everything in "key and value", key: value
+	// map store unique keys in sorted order
+
+	std::map<int, int>mp;
+	std::map<char, int>mpp;
+	std::map<std::string, int>mppp;
+
+	std::map<int, std::pair<int, int>>mpi;
+
+
+	// store 2 on the key = 1
+	mp[1] = 2;
+
+	// store 1 on the key = 3
+	mp.emplace({3, 1});
+
+	// store 4 on the key = 2
+	mp.insert({2, 4});
+
+
+	// print the map
+	for(auto it: mp)
+		std::cout << it.first << " " << it.second << '\n';
+
+
+
+}
+
+
+
+int main()
+{
+
+	return 0;
+}
