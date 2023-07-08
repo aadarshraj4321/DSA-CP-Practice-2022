8b0865987890b60b6c905a9ff63a97826f645bfd
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-07-08 16:27:20 +0530

    dsa

--- ds-algo-question/array-questions/3sum.cpp
+++ ds-algo-question/array-questions/3sum.cpp
@@ -29,12 +29,47 @@ std::vector<std::vector<int>>threeSumBrute(std::vector<int>arr)
 
 
 
+std::vector<std::vector<int>>threeSumBetter(std::vector<int>arr)
+{
+	std::set<std::vector<int>>st;
+	for(int i = 0; i < arr.size(); i++)
+	{
+		std::set<int>hashSet;
+		for(int j = i + 1; j < arr.size(); j++)
+		{
+			int thirdElement = -(arr[i] + arr[j]);
+			if(hashSet.find(thirdElement) != hashSet.end())
+			{
+				std::vector<int>temp = {arr[i], arr[j], thirdElement};
+				std::sort(temp.begin(), temp.end());
+				st.insert(temp);
+			}
+			hashSet.insert(arr[j]);
+		}
+	}
+
+	std::vector<std::vector<int>>finalResult(st.begin(), st.end());
+	return finalResult;
+}
+
+
+
 int main()
 {
 
 	std::vector<int>arr = {-1, -1, 2, 0, 1};
 	auto finalResult = threeSumBrute(arr);
 	for(auto it: finalResult)
+	{
+		for(auto i: it)
+			std::cout << i << " ";
+		std::cout << '\n';
+	}
+
+	std::cout << '\n';
+
+	auto finalResultBetter = threeSumBetter(arr);
+	for(auto it: finalResultBetter)
 	{
 		for(auto i: it)
 			std::cout << i << " ";
