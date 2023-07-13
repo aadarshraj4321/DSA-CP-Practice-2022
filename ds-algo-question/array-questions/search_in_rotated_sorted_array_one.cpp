b73bf3e6b70852ecad5c7fd20a4b793752422003
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-07-14 00:30:05 +0530

    dsa

---
+++ ds-algo-question/array-questions/search_in_rotated_sorted_array_one.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+
+
+
+
+int searchInRotSortArrayOne(std::vector<int>arr, int target)
+{
+	int left = 0;
+	int right = arr.size() - 1;
+	while(left <= right)
+	{
+		int mid = (left + right) / 2;
+		if(arr[mid] == target) return mid;
+
+		if(arr[left] <= arr[mid])
+		{
+			if(arr[left] <= target && target <= arr[mid])
+				right = mid - 1;
+			
+			else
+				left = mid + 1;
+		}
+		else
+		{
+			if(arr[mid] <= target && target <= arr[right])
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+	}
+	return -1;
+}
+
+
+
+
+
+
+int main()
+{
+	std::vector<int>arr = {4, 5, 6, 1, 2, 3};
+	std::cout << searchInRotSortArrayOne(arr, 6) << '\n';
+
+	return 0;
+}
