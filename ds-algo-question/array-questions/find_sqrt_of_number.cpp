1906475666c6fd0cc2c4e3d7b9d9949beb66d988
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-07-15 15:49:55 +0530

    binary_search

---
+++ ds-algo-question/array-questions/find_sqrt_of_number.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+
+
+
+int squareRootNumberBrute(int n)
+{
+	int ans = 1;
+	for(int i = 1; i <= n; i++)
+	{
+		if(i * i <= n) ans = i;
+		else break;
+	}
+	return ans;
+}
+
+
+
+int squareRootNumberOptimal(int n)
+{
+	int left = 1, right = n;
+	int ans = 1;
+
+	while(left <= right)
+	{
+		int mid = (left + right) / 2;
+		if(mid * mid <= n) 
+		{
+			ans = mid;
+			left = mid + 1;
+		}
+		else right = mid - 1;
+	}
+	return ans;
+}
+
+
+
+
+
+int main()
+{
+
+	int n = 28;
+	std::cout << squareRootNumberBrute(n) << '\n';
+	std::cout << squareRootNumberOptimal(n) << '\n';
+
+	return 0;
+}
