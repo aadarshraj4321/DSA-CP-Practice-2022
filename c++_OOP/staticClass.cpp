132c91d666f50d1cbb34b94d5366f80d95cecf54
aadarshraj4321 <aadarshraj4321@gmail.com>
2023-08-16 00:28:18 +0530

    OOPS

--- c++_OOP/staticClass.cpp
+++ c++_OOP/staticClass.cpp
@@ -8,6 +8,11 @@ class School
 		int rollNumber;
 
 		static int totalStudents;
+
+		School()
+		{
+			totalStudents++;
+		}
 };
 
 
@@ -15,12 +20,18 @@ int School :: totalStudents = 0;
 
 int main()
 {
-	School s1;
-	std::cout << s1.regNumber << " " << s1.rollNumber << '\n';
+	// School s1;
+	// std::cout << s1.regNumber << " " << s1.rollNumber << '\n';
 
-	s1.totalStudents = 11;
+	// s1.totalStudents = 11;
 
-	std::cout << School:: totalStudents << '\n';
+	// std::cout << School:: totalStudents << '\n';
+
+	School s1, s2, s3, s4;
+	std::cout << School :: totalStudents << '\n';
+
+	School *s5 = new School;
+	std::cout << School :: totalStudents << '\n';
 
 	return 0;
 }
